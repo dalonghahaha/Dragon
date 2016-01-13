f0b1c81de30f738153cc431274b705d3350acd34
dengjialong <dalonghahaha@163.com>
2016-01-13 19:19:26 +0800

    pool

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var Drangon ={};
 
 Drangon.Server = require('./lib/server');
 
-Drangon.Mongo = require('./lib/db/mongodb');
+Drangon.Mongo = require('./lib/orm/mongodb');
 
-Drangon.Mysql = require('./lib/db/mysql');
+Drangon.Mysql = require('./lib/orm/mysql');
 
-Drangon.Crypt = require('./lib/crypt');
+Drangon.Crypt = require('./lib/tool/crypt');
+
+Drangon.Async = require('./lib/tool/async');
 
 module.exports = Drangon;
