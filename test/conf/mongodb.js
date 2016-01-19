21ea7ee454a413ef3e69a6af5b03425b627dc0a4
dengjialong <dalonghahaha@163.com>
2016-01-19 16:08:15 +0800

    hide config

--- test/conf/mongodb.js
+++ test/conf/mongodb.js
@@ -10,17 +10,5 @@ module.exports = {
         'password': '',
         //数据库名
         'database': 'dalong'
-    },
-    'product': {
-        //主机名
-        'host': '114.215.106.133',
-        //端口
-        'port': 27017,
-        //用户名
-        'user': 'dalong',
-        //密码
-        'password': '200111',
-        //数据库名
-        'database': 'blog111'
     }
 }
--- test/conf/mysql.js
+++ test/conf/mysql.js
@@ -1,18 +1,6 @@
 module.exports = {
     'dev': {
         //主机名
-        'host': 'rdsnjiyzffruv6r.mysql.rds.aliyuncs.com',
-        //端口
-        'port': 3306,
-        //用户名
-        'user': 'db4rti7dnu24plfj',
-        //密码
-        'password': 'dalong200111',
-        //数据库名
-        'database': 'db4rti7dnu24plfj'
-    },
-    'product': {
-        //主机名
         'host': 'localhost',
         //端口
         'port': 3306,
